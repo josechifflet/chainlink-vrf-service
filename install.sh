e9cb95f1e85c3b11dfa554324c92552c1b3170f0
Jose Pedro Chifflet <josechifflet98@gmail.com>
2026-01-20 14:31:24 -0300

    🔧 Update install script to use --no-git flag for dependency installations

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-forge install foundry-rs/forge-std --no-commit && \
-forge install PaulRBerg/prb-test@release-v0 --no-commit && \
-forge install smartcontractkit/chainlink --no-commit && \
-forge install OpenZeppelin/openzeppelin-contracts@v5.0.2 --no-commit
+forge install foundry-rs/forge-std --no-git && \
+forge install PaulRBerg/prb-test@release-v0 --no-git && \
+forge install smartcontractkit/chainlink --no-git && \
+forge install OpenZeppelin/openzeppelin-contracts@v5.0.2 --no-git
